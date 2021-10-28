39a6cc9832bc59f73b8fa9fe73e264fb853547c8
GameGuyy <87716495+GameGuyy@users.noreply.github.com>
2021-10-28 12:18:15 +0530

    Minor visual changes

--- Assets/PlayScript.cs
+++ Assets/PlayScript.cs
@@ -5,11 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class PlayScript : MonoBehaviour
 {
+    public float waitforseconds = 3f;
     //public LevelManager levelManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(GameStart());
     }
 
     // Update is called once per frame
@@ -17,11 +18,14 @@ public class PlayScript : MonoBehaviour
     {
         
     }
-    public void Play()
+    
+    private IEnumerator GameStart()
     {
+        yield return new WaitForSeconds(waitforseconds);
         int sceneIndex = PlayerPrefs.GetInt("sceneIndex", 1);//
         //LevelManager level = new LevelManager();
         //StartCoroutine(level.LoadNextLevel());
         SceneManager.LoadScene(sceneIndex);
+
     }
 }
