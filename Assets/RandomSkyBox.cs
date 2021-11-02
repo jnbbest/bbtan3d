50a1d563d4960ace9f6977f0c3cbdd4a7a5172c2
GameGuyy <87716495+GameGuyy@users.noreply.github.com>
2021-11-02 14:24:57 +0530

    auto random skybox

---
+++ Assets/RandomSkyBox.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSkyBox : MonoBehaviour
+{
+
+    public Material[] skyboxes;
+    // Use this for initialization
+    void Start()
+    {
+        ChangeMySkybox();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void ChangeMySkybox()
+    {
+        int x = Random.Range(0, skyboxes.Length - 1);
+        RenderSettings.skybox = skyboxes[x];
+    }
+}
+    
+
